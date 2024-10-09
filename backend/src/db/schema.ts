1961db91301c9970928337a630a2f992dc4fa59e
Tran-Trung-Nhut <ttnhut904@gmail.com>
2024-10-09 11:13:00 +0700

    update schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,8 +1,23 @@
-import { varchar } from "drizzle-orm/pg-core";
+import { integer, varchar } from "drizzle-orm/pg-core";
 import { pgTable, uuid } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
+export const user = pgTable("user", {
+    id: uuid("id").defaultRandom().primaryKey(),
+    username: varchar("username", {length: 255}).notNull(),
+    password: varchar("password", {length: 255}).notNull(),
+    role: varchar("role", {length: 255}).notNull(),
+    name: varchar("name", {length: 255}).notNull(),
+    email: varchar("email", {length: 255}).notNull(),
+    phonenumber: varchar("phonenumber", {length: 255}).notNull(),
+    degree: varchar("degree", {length: 255}),
+})
+
+export const course = pgTable("course", {
     id: uuid("id").defaultRandom().primaryKey(),
     name: varchar("name", {length: 255}).notNull(),
-    email: varchar("email", {length: 255}).notNull()
+    price: integer("price").notNull(),
+    topic: varchar("name", {length: 255}).notNull(),
+    levelrequirement: varchar("levelrequirement", {length: 255}).notNull(),
+    description: varchar("description", {length: 255}),
+    paymentcode: varchar("paymentcode", {length: 255}).notNull(),
 })
