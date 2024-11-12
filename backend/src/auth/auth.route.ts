780a70ed6d98f21ecdb54aaab49872f4ca09bb65
Tran-Trung-Nhut <ttnhut904@gmail.com>
2024-11-12 20:08:10 +0700

    update remote db

--- backend/src/auth/auth.route.ts
+++ backend/src/auth/auth.route.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import authController from "./auth.controller";
-import userController from "../user/user.controller";
 import studentService from "../student/student.service";
+import studentController from "../student/student.controller";
 
 const router = Router()
 
 router.post('/login', authController.login as any)
-router.post('/register-as-student', studentService.createStudent as any)
+router.post('/register-as-student', studentController.createNewStudent as any)
 
 
 export default router
